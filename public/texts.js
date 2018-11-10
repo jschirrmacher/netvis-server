47fe2a2e39666903cd7916b893f3bd2d8b7b8998
Joachim Schirrmacher <j.schirrmacher@justso.de>
2018-11-10 16:33:40 +0100

    Fixed links to github project

--- public/texts.js
+++ public/texts.js
@@ -5,13 +5,13 @@ const allTexts = {
     "topics": "Themen",
     "interestedParties": "Interessierte Personen",
     "linkTitle": "{{title}} anzeigen",
-    "defaultDescription": "Es gibt aktuell noch keine Beschreibung zu diesem Eintrag. Informationen könntest du in [GitHub](https://github.com/jschirrmacher/netvis/blob/master/example/data.json) ergänzen."
+    "defaultDescription": "Es gibt aktuell noch keine Beschreibung zu diesem Eintrag. Informationen könntest du in [GitHub](https://github.com/jschirrmacher/netvis-server/blob/master/public/data.json) ergänzen."
   },
   "en": {
     "topics": "Topics",
     "interestedParties": "interested Parties",
     "linkTitle": "Show {{title}}",
-    "defaultDescription": "There is no description for this entry yet. You could add them in [GitHub](https://github.com/jschirrmacher/netvis/blob/master/example/data.json)."
+    "defaultDescription": "There is no description for this entry yet. You could add them in [GitHub](https://github.com/jschirrmacher/netvis-server/blob/master/public/data.json)."
   }
 }
 const texts = allTexts[lang] || allTexts[lang.substr(0, 2)] || allTexts[defaultLang]
