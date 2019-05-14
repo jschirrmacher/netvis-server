91cc63532e1306f50297cb2ce120cc77731a44e6
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-05-14 10:30:43 +0200

    Rooms contain their name now, so no need any more to generate one

--- fileReader.js
+++ fileReader.js
@@ -9,7 +9,6 @@ async function readData(fileName) {
   const data = JSON.parse(fs.readFileSync(fileName).toString())
   const headers = {'content-type': 'application/json'}
   await Object.keys(data).reduce((promise, roomId) => promise.then(async () => {
-    data[roomId].name = 'room_' + roomId
     data[roomId].id = roomId
     const body = JSON.stringify(data[roomId])
     const result = await fetch('http://localhost:3000/rooms', {method: 'post', body, headers})
